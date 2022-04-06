726266296cfba988927d3b80c459f539c57ce26b
Daniel Jr <daniel.souza.lima@outlook.com>
2022-04-06 10:20:25 -0300

    adding include and renaming function for rise_panic

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void panic(const char* error_msg) {
+#include "./utils.h"
+
+void rise_panic(const char* error_msg) {
   fprintf(stderr, "Error: { %s }\n", error_msg);
   exit(EXIT_FAILURE);
 }
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,6 +1,11 @@
+#ifndef SOCKET_CHAT_UTILS_H
+#define SOCKET_CHAT_UTILS_H
 
 /**
- * @brief output error_msg to stderr and exit program with code 1.
+ * @brief @brief output error_msg to stderr and exit program with code 1.
  * 
+ * @param error_msg 
  */
-void panic(const char* error_msg)
+void rise_panic(const char* error_msg);
+
+#endif
