0026a29a26376f6bd26fb7db4b6be3357cafff3c
Daniel Jr <daniel.souza.lima@outlook.com>
2022-05-02 11:57:10 -0300

    updated utils

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -3,7 +3,7 @@
 
 #include "./utils.h"
 
-void raise_panic(char* error_msg) {
+void raise_panic(char *error_msg) {
   fprintf(stderr, "Error: { %s }\n", error_msg);
   exit(EXIT_FAILURE);
 }
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -2,10 +2,10 @@
 #define SOCKET_CHAT_UTILS_H
 
 /**
- * @brief @brief output error_msg to stderr and exit program with code 1.
+ * @brief output error_msg to stderr and exit program with code 1.
  * 
  * @param error_msg 
  */
-void raise_panic(char* error_msg);
+void raise_panic(char *error_msg);
 
 #endif
